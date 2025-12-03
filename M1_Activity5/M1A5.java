7ad4a30039e95c64ebf943c1a62df735ff1fef54
Lou Bryan M. Pepano <LMPEPANO@robinsonsbank.com.ph>
2025-12-03 19:54:15 +0800

    initial upload

---
+++ M1_Activity5/M1A5.java
@@ -0,0 +1,17 @@
+package session1Activity1_01;
+
+public class M1A5 {
+
+    public static int computeSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int result = computeSum(50);
+        System.out.println("Sum = " + result);
+    }
+}
